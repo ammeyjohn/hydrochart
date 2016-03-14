4a35d4cae272f1478a4ccb79886e10ebe4c1a602
yuanjie <yuanjie@shanghai3h.com>
2016-03-14 18:02:34 +0800

    Create DrawArea object and create svg in it.

--- src/hydrochart.js
+++ src/hydrochart.js
@@ -10,25 +10,72 @@
             left: 10,
             bottom: 20,
             right: 10
+        },        
+        size: {
+            width: 800,
+            height: 600
         }
     }
 
+    // Defines the hydochart type
     var HydroChart = function(ele, option) {
-        this.version = '1.0';
+        this.version = '1.0';        
+
+        // Defines the alias name of this pointer.
+        var chart = this;        
 
         // Get the chart container
         if (isNullOrUndefine(ele)) {
-            this.element = d3.select('body');
+            chart.element = d3.select('body');
         } else {
             if (isString(ele)) {
-                this.element = d3.select(ele);
+                chart.element = d3.select(ele);
             } else {
-                this.element = ele;
+                chart.element = ele;
             }
-        }
+        }        
 
         // Get the chart option
-        this.option = $.extend({}, option, default_option);
+        chart.option = $.extend({}, option, default_option);
+
+        var size = chart.option.size;
+        if (isNullOrUndefine(size)) {  
+            var rect = chart.element.node().getBoundingClientRect()          
+            size.width = rect.width;
+            size.height = rect.height;
+        }        
+
+        // Create draw area object.
+        var area_option = {
+            size: {
+                width: size.width - chart.option.padding.left - chart.option.padding.right,
+                height: size.height - chart.option.padding.top - chart.option.padding.bottom,
+            }
+        }
+        chart.area = new DrawArea(chart, area_option);
+
+        //// Defines instance methods ////
+
+        chart.draw = function() {        
+            chart.area.draw(chart);
+        }        
+    }
+
+    // Defines the area type to draw shapes on.
+    var DrawArea = function(chart, option) {
+        
+        this.chart = chart;
+
+        // Compute the size of this draw area
+        this.size = option.size;
+    }
+
+    DrawArea.prototype.draw = function(){
+        this.svg = this.chart
+                       .element
+                       .append('svg')
+                       .attr('width', this.size.width)
+                       .attr('height', this.size.height);        
     }
 
     //// Defines the helper functions ////
